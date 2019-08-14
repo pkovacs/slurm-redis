e03a89d30f77926efa7ca9d845b273572cd6c7b7
Philip Kovacs <pkdevel@yahoo.com>
2019-08-14 10:04:13 -0400

    use auto deleters and _tmf in jobcomp_query
    Signed-off-by: Philip Kovacs <pkdevel@yahoo.com>

--- redis/jobcomp/jobcomp_query.c
+++ redis/jobcomp/jobcomp_query.c
@@ -33,22 +33,19 @@
 #include <string.h>
 
 #include "common/iso8601_format.h"
-#include "common/stringto.h"
+#include "jobcomp_auto.h"
 
 typedef struct job_query {
     RedisModuleCtx *ctx;
     RedisModuleString *err;
     const char *prefix;
     const char *uuid;
-    long long start_day;
-    long long end_day;
-#ifdef ISO8601_DATES
-    char start_time[ISO8601_SZ];
-    char end_time[ISO8601_SZ];
-#else
-    long long start_time;
-    long long end_time;
-#endif
+    // days since unix epoch
+    long long start_day, end_day;
+    // secs since unix epoch
+    long long start_time, end_time;
+    // iso8601 date/times w/tz "Z"
+    char start_time_c[ISO8601_SZ], end_time_c[ISO8601_SZ];
 } *job_query_t;
 
 job_query_t create_job_query(const job_query_init_t *init)
@@ -78,63 +75,75 @@ int job_query_prepare(job_query_t qry)
 {
     assert(qry != NULL);
 
-    // Open the primary query key
-    RedisModuleString *primary = RedisModule_CreateStringPrintf(qry->ctx,
-        "%s:qry:%s", qry->prefix, qry->uuid);
-    RedisModuleKey *primary_key = RedisModule_OpenKey(qry->ctx, primary,
-        REDISMODULE_READ);
-    if (RedisModule_KeyType(primary_key) == REDISMODULE_KEYTYPE_EMPTY) {
+    // Open the query key
+    AUTO_RMSTR redis_module_string_t query_keyname = {
+        .ctx = qry->ctx,
+        .str = RedisModule_CreateStringPrintf(qry->ctx, "%s:qry:%s",
+            qry->prefix, qry->uuid)
+    };
+    AUTO_RMKEY RedisModuleKey *query_key = RedisModule_OpenKey(qry->ctx,
+        query_keyname.str, REDISMODULE_READ);
+    if (RedisModule_KeyType(query_key) == REDISMODULE_KEYTYPE_EMPTY) {
         return QUERY_NULL;
     }
-    if (RedisModule_KeyType(primary_key) != REDISMODULE_KEYTYPE_HASH) {
+    if (RedisModule_KeyType(query_key) != REDISMODULE_KEYTYPE_HASH) {
         qry->err = RedisModule_CreateStringPrintf(qry->ctx,
             REDISMODULE_ERRORMSG_WRONGTYPE);
         return QUERY_ERR;
     }
 
-    // Fetch the scalar criteria on primary query key
-    RedisModuleString *start = NULL, *end = NULL;
-    if (RedisModule_HashGet(primary_key, REDISMODULE_HASH_CFIELDS, "Start",
-            &start, "End", &end, NULL) == REDISMODULE_ERR) {
+    // Fetch criteria on the query key
+    AUTO_RMSTR redis_module_string_t tmf = { .ctx = qry->ctx };
+    AUTO_RMSTR redis_module_string_t start = { .ctx = qry->ctx };
+    AUTO_RMSTR redis_module_string_t end = { .ctx = qry->ctx };
+    if (RedisModule_HashGet(query_key, REDISMODULE_HASH_CFIELDS, "_tmf",
+            &tmf.str, "Start", &start.str, "End", &end.str, NULL)
+        == REDISMODULE_ERR) {
         qry->err = RedisModule_CreateStringPrintf(qry->ctx,
-            "expected key(s) missing");
+            "error fetching query data");
         return QUERY_ERR;
     }
-    RedisModule_CloseKey(primary_key);
 
-    const char *start_c = RedisModule_StringPtrLen(start, NULL);
-    const char *end_c = RedisModule_StringPtrLen(end, NULL);
+    long long _tmf;
+    if (RedisModule_StringToLongLong(tmf.str, &_tmf) == REDISMODULE_ERR) {
+        RedisModule_ReplyWithError(qry->ctx, "invalid _tmf");
+        return QUERY_ERR;
+    }
+
     long long start_time, end_time;
-
-#ifdef ISO8601_DATES
-    start_time = mk_time(start_c);
-    if (start_time == (-1)) {
-        qry->err = RedisModule_CreateStringPrintf(qry->ctx,
-            "invalid iso8601 start date/time");
-        return QUERY_ERR;
+    if (_tmf == 1) {
+        const char *start_c = RedisModule_StringPtrLen(start.str, NULL);
+        const char *end_c = RedisModule_StringPtrLen(end.str, NULL);
+        start_time = mk_time(start_c);
+        if (start_time == (-1)) {
+            qry->err = RedisModule_CreateStringPrintf(qry->ctx,
+                "invalid iso8601 start date/time");
+            return QUERY_ERR;
+        }
+        end_time = mk_time(end_c);
+        if (end_time == (-1)) {
+            qry->err = RedisModule_CreateStringPrintf(qry->ctx,
+                "invalid iso8601 end date/time");
+            return QUERY_ERR;
+        }
+        strncpy(qry->start_time_c, start_c, ISO8601_SZ-1);
+        strncpy(qry->end_time_c, end_c, ISO8601_SZ-1);
+    } else {
+        if (RedisModule_StringToLongLong(start.str, &start_time)
+            == REDISMODULE_ERR) {
+            qry->err = RedisModule_CreateStringPrintf(qry->ctx,
+                "invalid start time");
+            return QUERY_ERR;
+        }
+        if (RedisModule_StringToLongLong(end.str, &end_time)
+            == REDISMODULE_ERR) {
+            qry->err = RedisModule_CreateStringPrintf(qry->ctx,
+                "invalid end time");
+            return QUERY_ERR;
+        }
+        qry->start_time = start_time;
+        qry->end_time = end_time;
     }
-    end_time = mk_time(end_c);
-    if (end_time == (-1)) {
-        qry->err = RedisModule_CreateStringPrintf(qry->ctx,
-            "invalid iso8601 end date/time");
-        return QUERY_ERR;
-    }
-    strncpy(qry->start_time, start_c, ISO8601_SZ-1);
-    strncpy(qry->end_time, end_c, ISO8601_SZ-1);
-#else
-    if (sr_strtoll(start_c, &start_time) < 0) {
-        qry->err = RedisModule_CreateStringPrintf(qry->ctx,
-            "invalid start time");
-        return QUERY_ERR;
-    }
-    if (sr_strtoll(end_c, &end_time) < 0) {
-        qry->err = RedisModule_CreateStringPrintf(qry->ctx,
-            "invalid end time");
-        return QUERY_ERR;
-    }
-    qry->start_time = start_time;
-    qry->end_time = end_time;
-#endif
     qry->start_day = start_time / SECONDS_PER_DAY;
     qry->end_day = end_time / SECONDS_PER_DAY;
 
@@ -156,11 +165,14 @@ int job_query_match_job(const job_query_t qry, long long jobid)
     assert(qry != NULL);
     assert(jobid > 0);
 
-    // Open job key
-    RedisModuleString *job_s = RedisModule_CreateStringPrintf(qry->ctx,
-        "%s:%lld", qry->prefix, jobid);
-    RedisModuleKey *job_key = RedisModule_OpenKey(qry->ctx, job_s,
-        REDISMODULE_READ);
+    // Open the job key
+    AUTO_RMSTR redis_module_string_t job_keyname = {
+        .ctx = qry->ctx,
+        .str = RedisModule_CreateStringPrintf(qry->ctx, "%s:%lld",
+            qry->prefix, jobid)
+    };
+    AUTO_RMKEY RedisModuleKey *job_key = RedisModule_OpenKey(qry->ctx,
+        job_keyname.str, REDISMODULE_READ);
     if (RedisModule_KeyType(job_key) == REDISMODULE_KEYTYPE_EMPTY) {
         return QUERY_NULL;
     }
@@ -170,36 +182,45 @@ int job_query_match_job(const job_query_t qry, long long jobid)
         return QUERY_ERR;
     }
 
-    // Fetch job data on job key
-    RedisModuleString *start = NULL, *end = NULL;
-    if (RedisModule_HashGet(job_key, REDISMODULE_HASH_CFIELDS, "Start", &start,
-            "End", &end, NULL) == REDISMODULE_ERR) {
+    // Fetch data on the job key
+    AUTO_RMSTR redis_module_string_t tmf = { .ctx = qry->ctx };
+    AUTO_RMSTR redis_module_string_t start = { .ctx = qry->ctx };
+    AUTO_RMSTR redis_module_string_t end = { .ctx = qry->ctx };
+    if (RedisModule_HashGet(job_key, REDISMODULE_HASH_CFIELDS, "_tmf",
+            &tmf.str, "Start", &start.str, "End", &end.str, NULL)
+        == REDISMODULE_ERR) {
         qry->err = RedisModule_CreateStringPrintf(qry->ctx,
-            "error fetching job");
+            "error fetching job data");
         return QUERY_ERR;
     }
-    RedisModule_CloseKey(job_key);
 
-    const char *start_c = RedisModule_StringPtrLen(start, NULL);
-    const char *end_c = RedisModule_StringPtrLen(end, NULL);
+    long long _tmf;
+    if (RedisModule_StringToLongLong(tmf.str, &_tmf) == REDISMODULE_ERR) {
+        RedisModule_ReplyWithError(qry->ctx, "invalid _tmf");
+        return QUERY_ERR;
+    }
 
-#ifdef ISO8601_DATES
-    if (strncmp(qry->start_time, start_c, ISO8601_SZ-1) > 0 ||
-            strncmp(qry->end_time, end_c, ISO8601_SZ-1) < 0) {
-        return QUERY_FAIL;
+    if (_tmf == 1) {
+        const char *start_c = RedisModule_StringPtrLen(start.str, NULL);
+        const char *end_c = RedisModule_StringPtrLen(end.str, NULL);
+        if (strncmp(qry->start_time_c, start_c, ISO8601_SZ-1) > 0 ||
+                strncmp(qry->end_time_c, end_c, ISO8601_SZ-1) < 0) {
+            return QUERY_FAIL;
+        }
+    } else {
+        long long start_time, end_time;
+        if (RedisModule_StringToLongLong(start.str, &start_time)
+            == REDISMODULE_ERR) {
+            return QUERY_FAIL;
+        }
+        if (RedisModule_StringToLongLong(end.str, &end_time)
+            == REDISMODULE_ERR) {
+            return QUERY_FAIL;
+        }
+        if (qry->start_time > start_time || qry->end_time < end_time) {
+            return QUERY_FAIL;
+        }
     }
-#else
-    long long start_time, end_time;
-    if (sr_strtoll(start_c, &start_time) < 0) {
-        return QUERY_FAIL;
-    }
-    if (sr_strtoll(end_c, &end_time) < 0) {
-        return QUERY_FAIL;
-    }
-    if (qry->start_time > start_time || qry->end_time < end_time) {
-        return QUERY_FAIL;
-    }
-#endif
 
     return QUERY_PASS;
 }
